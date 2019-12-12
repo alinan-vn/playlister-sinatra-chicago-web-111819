5c1825c6788329b0cafbd6e62e07404539308832
github username <github email address>
2019-12-12 12:19:18 -0600

    Added SongGenre table, fixed some column names... Added data to Seeds file BUT did not rake the data yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_170057) do
+ActiveRecord::Schema.define(version: 2019_12_12_174007) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -19,14 +19,20 @@ ActiveRecord::Schema.define(version: 2019_12_12_170057) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.integer "Song_id"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "songs", force: :cascade do |t|
-    t.integer "Artist_id"
+    t.integer "artist_id"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
